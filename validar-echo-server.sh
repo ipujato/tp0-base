39735b79e60bfe1e5da968942651c74c5882d8c1
ipuj <ipujato@fi.uba.ar>
2025-03-16 19:34:31 -0300

    [DONE} tests ok. Faltaba el TO.

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-RESPUESTA=$(echo "Mensaje ej3" | docker run --rm --platform linux/amd64 --network=tp0_testing_net -i subfuzion/netcat -w 3 server 12345)
+RESPUESTA=$(echo "Mensaje comprobacion ej3" | docker run --rm --platform linux/amd64 --network=tp0_testing_net -i subfuzion/netcat -w 3 server 12345)
 
-if [ "$RESPUESTA" = "Mensaje ej3" ]; then
+if [ "$RESPUESTA" = "Mensaje comprobacion ej3" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
