f98acd03f63bf5e61d04f8cc379d7df7321e2104
ipuj <ipujato@fi.uba.ar>
2025-03-16 19:18:46 -0300

    [fix] pipe al echo

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#https://hub.docker.com/r/subfuzion/netcat
-
-echo "Mensaje ej3"
-RESPUESTA=$(docker run --rm --platform linux/amd64 --network=testing_net -i subfuzion/netcat server 12345)
+RESPUESTA=$(echo "Mensaje ej3" | docker run --rm --platform linux/amd64 --network=testing_net -i subfuzion/netcat server 12345)
 
 if [ "$RESPUESTA" = "Mensaje ej3" ]; then
   echo "action: test_echo_server | result: success"
