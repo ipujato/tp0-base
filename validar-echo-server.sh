7c3d5867f57400195e135024efb335a6b94b1e52
ipuj <ipujato@fi.uba.ar>
2025-03-16 18:56:33 -0300

    [fix] nombre de red

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -3,7 +3,7 @@
 #https://hub.docker.com/r/subfuzion/netcat
 
 echo "Mensaje ej3"
-RESPUESTA=$(docker run -t --rm --network=testnet --name=ncs subfuzion/netcat -vl 12345)
+RESPUESTA=$(docker run -t --rm --network=testing_net --name=ncs subfuzion/netcat -vl 12345)
 
 if [ "$RESPUESTA" = "Mensaje ej3" ]; then
   echo "action: test_echo_server | result: success"
