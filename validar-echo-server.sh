b2781e026866a5e585d313bd09e463e4a648a6f6
ipuj <ipujato@fi.uba.ar>
2025-03-16 19:25:51 -0300

    [feat] corrio bien local. falta test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESPUESTA=$(echo "Mensaje ej3" | docker run --rm --platform linux/amd64 --network=testing_net -i subfuzion/netcat server 12345)
+RESPUESTA=$(echo "Mensaje ej3" | docker run --rm --platform linux/amd64 --network=tp0_testing_net -i subfuzion/netcat server 12345)
 
 if [ "$RESPUESTA" = "Mensaje ej3" ]; then
   echo "action: test_echo_server | result: success"
