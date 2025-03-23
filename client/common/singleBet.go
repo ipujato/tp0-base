5d3b5581714f06bae80028a29f9edf3ffa4b499e
ipuj <ipujato@fi.uba.ar>
2025-03-23 19:01:15 -0300

    remove the log molesto

--- client/common/singleBet.go
+++ client/common/singleBet.go
@@ -18,6 +18,5 @@ func (b Bet) getBetSize() uint32 {
 }
 
 func (b Bet) getBetSerialized() string {
-	log.Infof("bet con %s|%s|%s|%s|%s|%s", b.Agencia, b.Nombre, b.Apellido, b.Documento, b.Nacimiento, b.Numero)
 	return fmt.Sprintf("%s|%s|%s|%s|%s|%s", b.Agencia, b.Nombre, b.Apellido, b.Documento, b.Nacimiento, b.Numero)
 }
