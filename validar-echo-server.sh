e7d6046a40aa9565863209aaf36f0b0bf3f46139
ipuj <ipujato@fi.uba.ar>
2025-03-26 22:42:00 -0300

    FIX CLIENTE NO SERVER

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-RESPUESTA=$(echo "Mensaje comprobacion ej3" | docker run --rm --platform linux/amd64 --network=tp0_testing_net -i subfuzion/netcat -w 3 server 12345)
+TEST_MESSAGE="Mensaje comprobacion ej3"
 
-if [ "$RESPUESTA" = "Mensaje comprobacion ej3" ]; then
+RESPUESTA=$(echo "$TEST_MESSAGE" | docker run --rm --platform linux/amd64 --network=tp0_testing_net alpine sh -c "nc server 12345 -w 3")
+
+if [ "$RESPUESTA" = "$TEST_MESSAGE" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
