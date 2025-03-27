8b74d62fd264af6dc80688bdce6a039a87635eab
ipuj <ipujato@fi.uba.ar>
2025-03-26 22:44:18 -0300

    caso faltante la librerira

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! docker image inspect alpine >/dev/null 2>&1; then
+  echo "Alpine no encontrado. Descargándolo..."
+  docker pull alpine
+fi
+
 TEST_MESSAGE="Mensaje comprobacion ej3"
 
 RESPUESTA=$(echo "$TEST_MESSAGE" | docker run --rm --platform linux/amd64 --network=tp0_testing_net alpine sh -c "nc server 12345 -w 3")
