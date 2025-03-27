ee3bef1951e028f547b61db9ec62272068935a67
ipuj <ipujato@fi.uba.ar>
2025-03-26 23:52:45 -0300

    fix echo server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-RESPUESTA=$(echo "Mensaje comprobacion ej3" | docker run --rm --platform linux/amd64 --network=tp0_testing_net -i subfuzion/netcat -w 3 server 12345)
+if ! docker image inspect alpine >/dev/null 2>&1; then
+  echo "Alpine no encontrado. Descargándolo..."
+  docker pull alpine
+fi
 
-if [ "$RESPUESTA" = "Mensaje comprobacion ej3" ]; then
+MENSAJE="Mensaje comprobacion ej3"
+
+RESPUESTA=$(docker run --rm --platform linux/amd64 --network=tp0_testing_net alpine sh -c "echo $MENSAJE | nc server 12345")
+
+if [ "$RESPUESTA" = "$MENSAJE" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
